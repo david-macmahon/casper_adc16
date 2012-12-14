a811816c7b32d2191065b98c379178ec6c6b7b14
David MacMahon <davidm@astro.berkeley.edu>
2012-12-13 21:18:37 -0800

    Allow snap to accept ADCs in other forms
    Instead of only supporting Symbols (e.g. :a), snap now allows the ADC(s)
    to be specified as numbers (0-3) or Strings ('a' to 'd' or 'A' to 'D').

--- ruby/lib/adc16.rb
+++ ruby/lib/adc16.rb
@@ -236,10 +236,25 @@ end # class ADC16
 # Class for communicating with snap and trig blocks of adc16_test model.
 class ADC16Test < ADC16
 
-  # For each chip given in +chips+ (one or more of :a, :b, :c, :d), a 64K
-  # NArray is returned.
+  # For each chip given in +chips+ (one or more of :a to :d, 0 to 3, 'a' to
+  # 'd', or 'A' to 'D'), a 64K NArray is returned.  A trailing Hash argument
+  # can specify the leth to snap via the :n key.
   def snap(*chips)
-    len = (Fixnum === chips[-1]) ? chips.pop : 1<<16
+    # A trailing Hash argument can be passed for options
+    opts = (Hash === chips[-1]) ? chips.pop : {}
+    len = opts[:n] || (1<<16)
+
+    # Convert chips to symbols
+    chips.map! do |chip|
+      case chip
+      when 0, :a, 'a', 'A'; :a
+      when 1, :b, 'b', 'B'; :b
+      when 2, :c, 'c', 'C'; :c
+      when 3, :d, 'd', 'D'; :d
+      else raise "Invalid chip: #{chip}"
+      end
+    end
+
     self.trig = 0
     chips.each do |chip|
       # snap_x_ctrl bit 0: 0-to-1 = enable
@@ -250,17 +265,17 @@ class ADC16Test < ADC16
       # Due to tcpborphserver3 bug, writes to the control registers must be
       # done using the KATCP ?wordwrite command.  See the email thread at
       # http://www.mail-archive.com/casper@lists.berkeley.edu/msg03457.html
-      request(:wordwrite, "snap_#{chip.to_s.downcase}_ctrl", 0, 0b0000);
-      request(:wordwrite, "snap_#{chip.to_s.downcase}_ctrl", 0, 0b0101);
+      request(:wordwrite, "snap_#{chip}_ctrl", 0, 0b0000);
+      request(:wordwrite, "snap_#{chip}_ctrl", 0, 0b0101);
     end
     self.trig = 1
     sleep 0.01
     self.trig = 0
     chips.each do |chip|
-      request(:wordwrite, "snap_#{chip.to_s.downcase}_ctrl", 0, 0b0000);
+      request(:wordwrite, "snap_#{chip}_ctrl", 0, 0b0000);
     end
     out = chips.map do |chip|
-      send("snap_#{chip.to_s.downcase}_bram")[0,len]
+      send("snap_#{chip}_bram")[0,len]
     end
     chips.length == 1 ? out[0] : out
   end
