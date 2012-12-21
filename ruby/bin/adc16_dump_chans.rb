63a2c4edc2913f0aeb033ce8216b09dfa9e75d0e
David MacMahon <davidm@astro.berkeley.edu>
2012-12-21 13:38:08 -0800

    Dump from adc16_test 64K snapshot buffers

--- ruby/bin/adc16_dump_chans.rb
+++ ruby/bin/adc16_dump_chans.rb
@@ -1,29 +1,33 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
-require 'adc16'
+require 'adc16/test'
 
 # TODO Get these from command line
 opts = {
-  :nsamps => (1<<4)
+  :nsamps => (1<<16),
+  :verbose => false
 }
 
 raise "\nusage: #{File.basename $0} R2HOSTNAME" unless ARGV[0]
 
-a = ADC16.new(ARGV[0])
+a = ADC16Test.new(ARGV[0])
 
-data = ['A', 'B', 'C', 'D'].map do |chip|
-  a.snap(chip, :n => opts[:nsamps])
-end
+tic = Time.now
+data = a.snap_test(:a, :b, :c, :d, :n => opts[:nsamps])
+toc = Time.now
+$stderr.puts "data snap took #{toc-tic} seconds" if opts[:verbose]
 
 fmt = (['%4d'] * 16).join(' ') + "\n"
 
+tic = Time.now
 opts[:nsamps].times do |i|
-  4.times do |adc|
-    4.times do |chan|
-      print ' ' unless adc==0 && chan==0
-      printf '%4d', data[adc][chan,i]
-    end
-  end
-  puts
+  printf(fmt,
+         data[0][0,i], data[0][1,i], data[0][2,i], data[0][3,i],
+         data[1][0,i], data[1][1,i], data[1][2,i], data[1][3,i],
+         data[2][0,i], data[2][1,i], data[2][2,i], data[2][3,i],
+         data[3][0,i], data[3][1,i], data[3][2,i], data[3][3,i],
+        )
 end
+toc = Time.now
+$stderr.puts "data dump took #{toc-tic} seconds" if opts[:verbose]
