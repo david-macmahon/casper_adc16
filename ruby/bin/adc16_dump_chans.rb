23a7b96c82092a70f9ec0c0ab81b0800e6bcb8fa
David MacMahon <davidm@astro.berkeley.edu>
2013-01-28 12:55:35 -0800

    Add RMS output option to adc16_dump_chans.rb

--- ruby/bin/adc16_dump_chans.rb
+++ ruby/bin/adc16_dump_chans.rb
@@ -6,6 +6,7 @@ require 'adc16/test'
 
 OPTS = {
   :nsamps => (1<<16),
+  :rms => false,
   :verbose => false
 }
 
@@ -19,6 +20,9 @@ OP = OptionParser.new do |o|
   o.separator 'Options:'
   o.on('-l', '--length=N', Integer, "Number of samples to dump per channel [#{OPTS[:nsamps]}]") do |o|
     OPTS[:nsamps] = o
+  end
+  o.on('-r', '--rms', "Output RMS of each channel instead of raw samples [#{OPTS[:rms]}]") do |o|
+    OPTS[:rms] = o
   end
   o.on('-v', '--[no-]verbose', "Display more info [#{OPTS[:verbose]}]") do |o|
     OPTS[:verbose] = o
@@ -35,6 +39,28 @@ if ARGV.empty?
   exit 1
 end
 
+def dump_samples(data)
+  fmt = (['%4d'] * 16).join(' ') + "\n"
+  tic = Time.now
+  OPTS[:nsamps].times do |i|
+    printf(fmt,
+           data[0][0,i], data[0][1,i], data[0][2,i], data[0][3,i],
+           data[1][0,i], data[1][1,i], data[1][2,i], data[1][3,i],
+           data[2][0,i], data[2][1,i], data[2][2,i], data[2][3,i],
+           data[3][0,i], data[3][1,i], data[3][2,i], data[3][3,i],
+          )
+  end
+  toc = Time.now
+  $stderr.puts "data dump took #{toc-tic} seconds" if OPTS[:verbose]
+end
+
+def dump_rms(data)
+  fmt = (['%4.1f'] * 16).join(' ') + "\n"
+  rms = data.map {|na| na.rms(1).to_a}
+  rms.flatten!
+  printf(fmt, *rms)
+end
+
 a = ADC16Test.new(ARGV[0])
 
 tic = Time.now
@@ -43,16 +69,8 @@ data = a.snap_test(:a, :b, :c, :d, :n => OPTS[:nsamps])
 toc = Time.now
 $stderr.puts "data snap took #{toc-tic} seconds" if OPTS[:verbose]
 
-fmt = (['%4d'] * 16).join(' ') + "\n"
-
-tic = Time.now
-OPTS[:nsamps].times do |i|
-  printf(fmt,
-         data[0][0,i], data[0][1,i], data[0][2,i], data[0][3,i],
-         data[1][0,i], data[1][1,i], data[1][2,i], data[1][3,i],
-         data[2][0,i], data[2][1,i], data[2][2,i], data[2][3,i],
-         data[3][0,i], data[3][1,i], data[3][2,i], data[3][3,i],
-        )
+if OPTS[:rms]
+  dump_rms(data)
+else
+  dump_samples(data)
 end
-toc = Time.now
-$stderr.puts "data dump took #{toc-tic} seconds" if OPTS[:verbose]
